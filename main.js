abdc35b0478ca4723e43536e790269467391abf8
Hengxi <jinhengxi93@gmail.com>
2022-04-07 18:13:16 +0900

    JS home typing

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
-
 document.addEventListener('scroll',()=>{
     if( window.scrollY > navbarHeight ){
         navbar.classList.add('navbar-dark');
@@ -10,7 +9,6 @@ document.addEventListener('scroll',()=>{
         navbar.classList.remove('navbar-dark')
     };
 });
-
 navbar.addEventListener('click',(e)=>{
     const target = e.target.dataset.link;
     if( target == null ){
@@ -28,3 +26,45 @@ function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView( {behavior : 'smooth'} );
 }
+
+
+
+const home = document.querySelector('.home-title');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll',()=>{
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+
+
+const hello = document.querySelector('.hello');
+
+function selectArr(){
+    const string = "Hello, I'm Hengxi"
+    const stringArr = string.split('')
+    console.log(stringArr)
+    return stringArr;
+}
+
+function resetHello(){
+    hello.textContent = '';
+    dynamic(selectArr());
+}
+
+function dynamic(Arr){
+    if( Arr.length > 0 ){
+        hello.textContent += Arr.shift()
+        setTimeout(()=>{
+            dynamic(Arr)
+        },80)
+    }else{
+        setTimeout(()=>{
+            resetHello()
+        },2000)
+    }
+}
+dynamic(selectArr())
+
+// setInterval(()=>{
+//     hello.classList.toggle('active')
+// },300)
